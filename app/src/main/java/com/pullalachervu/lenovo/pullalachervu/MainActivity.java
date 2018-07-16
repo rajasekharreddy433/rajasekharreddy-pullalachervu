88b8a9d0a9bf2755257ae2c8a2390e643fa35fe1
rajasekhar <ifoodhubdev@gmail.com>
2018-07-16 23:10:52 +0530

    first commit

---
+++ app/src/main/java/com/pullalachervu/lenovo/pullalachervu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pullalachervu.lenovo.pullalachervu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
